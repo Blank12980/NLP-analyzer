26f53d179c58bf3e32dcdf554b63bb1350bb8f43
ILiam <167431501+Blank12980@users.noreply.github.com>
2025-02-23 11:42:20 +1000

    add the comment to the cod

--- src/functions.py
+++ src/functions.py
@@ -10,68 +10,81 @@ import nltk
 import re
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-nltk.download('stopwords')
-nltk.download('wordnet')
-# work with data
-with open('./src/dataset.txt','r',encoding='utf-8') as file:
-    text = file.readlines() # Определяем стоп-слова
 
-for i in range(len(text)):
-    text[i] = text[i].strip().split(';') if len(text[i].strip().split(';')) > 1 else None
+class trains():
+    # load the data of stopwords  and wordnet 
+    nltk.download('stopwords')
+    nltk.download('wordnet')
+    
+    def loadTheData():
+        """ load and convert the data """
+        with open('./src/dataset.txt','r',encoding='utf-8') as file:
+            text = file.readlines() 
 
-try: text.remove(None)
-except: ''
+        for i in range(len(text)):
+            text[i] = text[i].strip().split(';') if len(text[i].strip().split(';')) > 1 else None
+
+        try: text.remove(None)
+        except: ''
+        return text
 
 
-def convert_text(text):
-    stop_words = set(stopwords.words('english')) 
-    lemmatizer = WordNetLemmatizer()
-    text = text.lower()  
-    text = re.sub(r'[^\w\s]', '', text)
-    text = ' '.join(word for word in text.split() if word not in stop_words) 
-    lemmatizer = WordNetLemmatizer()
-    text = ' '.join(lemmatizer.lemmatize(word) for word in text.split()) 
-    return text
-data =  [{"text": convert_text(i[0]),  "emotion": i[1]  }for i in text]
-df = pd.DataFrame(data)
+    def convert_text(text: str):
+        """ convert the text to better for model """
+        stop_words = set(stopwords.words('english')) 
+        lemmatizer = WordNetLemmatizer()
+        text = text.lower()  
+        text = re.sub(r'[^\w\s]', '', text)
+        text = ' '.join(word for word in text.split() if word not in stop_words) 
+        lemmatizer = WordNetLemmatizer()
+        text = ' '.join(lemmatizer.lemmatize(word) for word in text.split()) 
+        return text
 
-# tokenization
-tokenizer = Tokenizer()
-tokenizer.fit_on_texts(df["text"])
-X = tokenizer.texts_to_sequences(df["text"])
-maxlen = 10000
-X = pad_sequences(X,maxlen=maxlen, padding='post')
+    def tran(text: str):
+        ''' Start the train '''
+        data =  [{"text": trains.convert_text(text=str(i[0])),  "emotion": i[1]  }for i in text]
+        df = pd.DataFrame(data)
 
-label_encoder = LabelEncoder()
-y = label_encoder.fit_transform(df["emotion"])
+        # tokenization
+        tokenizer = Tokenizer()
+        tokenizer.fit_on_texts(df["text"])
+        X = tokenizer.texts_to_sequences(df["text"])
+        maxlen = 10000
+        X = pad_sequences(X,maxlen=maxlen, padding='post')
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=2, random_state=192)
+        label_encoder = LabelEncoder()
+        y = label_encoder.fit_transform(df["emotion"])
 
-# create NLP
-model = Sequential()
-model.add(Embedding(input_dim=len(tokenizer.word_index) + 1, output_dim=128, input_length=maxlen))
-model.add(Conv1D(filters=128, kernel_size=5, activation='relu'))
-model.add(MaxPooling1D(pool_size=2))
-model.add(Conv1D(filters=256, kernel_size=5, activation='relu'))
-model.add(MaxPooling1D(pool_size=2))
-model.add(GlobalMaxPooling1D())
-model.add(Dropout(0.5))
-model.add(Dense(256, activation='relu'))
-model.add(Dense(len(label_encoder.classes_), activation='softmax'))
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=2, random_state=192)
 
-# compile the model
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        # create NLP
+        model = Sequential()
+        model.add(Embedding(input_dim=len(tokenizer.word_index) + 1, output_dim=128, input_length=maxlen))
+        model.add(Conv1D(filters=128, kernel_size=5, activation='relu'))
+        model.add(MaxPooling1D(pool_size=2))
+        model.add(Conv1D(filters=256, kernel_size=5, activation='relu'))
+        model.add(MaxPooling1D(pool_size=2))
+        model.add(GlobalMaxPooling1D())
+        model.add(Dropout(0.5))
+        model.add(Dense(256, activation='relu'))
+        model.add(Dense(len(label_encoder.classes_), activation='softmax'))
 
-# start the learn
-model.fit(X_train, y_train, epochs=5, batch_size=4, validation_data=(X_test, y_test),shuffle=True)
+        # compile the model
+        model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# save part
-model.save('./src/model.h5')
-with open('./src/tokenizer.pkl', 'wb') as f:
-    pickle.dump(tokenizer, f)
+        # start the learn
+        model.fit(X_train, y_train, epochs=5, batch_size=4, validation_data=(X_test, y_test),shuffle=True)
 
-with open('./src/encoder.pkl', 'wb') as f:
-    pickle.dump(label_encoder, f)
+        # save part
+        model.save('./src/model.h5')
+        with open('./src/tokenizer.pkl', 'wb') as f:
+            pickle.dump(tokenizer, f)
 
-print("save to ./src/model.h5.")
+        with open('./src/encoder.pkl', 'wb') as f:
+            pickle.dump(label_encoder, f)
 
+        print("save to ./src/model.h5.")
+
+if __name__ == '__main__':
+    text = trains.loadTheData()
+    trains.tran(text=text)
